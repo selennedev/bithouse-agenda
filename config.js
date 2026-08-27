e29e4083755de57a2300e21afd60fd2a725ce033
Selenne Dev <265148682+selennedev@users.noreply.github.com>
2026-08-27 09:28:02 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.BITHOUSE_SUPABASE_URL = "https://seu-projeto.supabase.co";
-window.BITHOUSE_SUPABASE_KEY = "sua-chave-publicável";
+window.BITHOUSE_SUPABASE_URL = "https://cxiyamirmygkWxrtavnl.supabase.co";
+window.BITHOUSE_SUPABASE_KEY = "sb_publishable_opYIyBDyYJ9av58IFeuGig_i1WFnS2D";
